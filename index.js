c97ddfd560c0b27af9f08a801706519e062cca09
OmarIsComing <d.lord2022@gmail.com>
2025-07-17 00:58:24 +0330

    add ui to project

--- index.js
+++ index.js
@@ -17,46 +17,86 @@ const getComputerChoice = () => {
     const randomChoice = Math.floor(Math.random() * 3) + 1
     return numberToString[randomChoice]
 }
-
-const getHumanChoice = () => {
-    const input = prompt('Choose: ROCK / PAPER / SCISSORS ?');
-    return input ? input.toLowerCase() : '';
-    }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 const playRound = (humanChoice, computerChoice) => {
-
-    console.log(`YOURT CHOICE: ${humanChoice}
-
-COMPUTER CHOICE: ${computerChoice}`)
-
-
     if (humanChoice === computerChoice) {
-        console.log('No One Win! SAME')
+        return 'No One Win! SAME'
     } else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
         (humanChoice === 'paper' && computerChoice === 'rock') ||
-        (humanChoice === 'scissors' && computerChoice === 'rock')) {
-        console.log("YOU WON!")
+        (humanChoice === 'scissors' && computerChoice === 'paper')) {
         ++humanScore;
+        return "YOU WON!"
     }
     else {
-        console.log("COMPUTER WON!")
         ++computerScore;
+        return "COMPUTER WON!"
     }
 }
 
-const playGame = () => {
 
-    for (let i = 0; i < 5; i++) {
-        const computerSelection = getComputerChoice();
-        const humanSelection = getHumanChoice();    
-        playRound(humanSelection, computerSelection)
-    }
+let clickDisabled = false;
+let restClickDisabled = false;
+let i = 1;
+async function logScreenText() {
+    console.log(i)
+    if (clickDisabled) return;
+    clickDisabled = true;
+    restClickDisabled = true;
 
-    if (humanScore > computerScore) {
-        console.log("FINAL WINNER: YOU!")
-    } else {
-        console.log("FINAL WINNER: COMPUTER!!")
+    // Play one round
+    const userChoose = this.className.split(' ')[1];
+    screen.innerHTML = `<p class="screen-text">You Choose: ${userChoose}!</p>`;
+    await sleep(2000);
+
+    const computerChoose = await getComputerChoice();
+    screen.innerHTML = `<p class="screen-text">Computer Choose: ${computerChoose}!</p>`;
+    await sleep(2000);
+
+    const result = playRound(userChoose, computerChoose);
+    screen.innerHTML = `<p class="screen-text">${result}!</p>`;
+
+    if (i >= 5) {
+        const score =  `YOU(${humanScore}) VS COMPUTER(${computerScore})`
+        if (humanScore > computerScore) {
+            screen.innerHTML =`<p class="screen-text">FINAL WINNER: YOU! <br>${score}</p>`
+        } else if (computerScore > humanScore) {
+            screen.innerHTML = `<p class="screen-text">FINAL WINNER: COMPUTER!<br>${score}</p>`
+        }
+        clickDisabled = true;
+        restClickDisabled = false;
+
+        return;
     }
+    ++i; // increment round count after playing
+
+    clickDisabled = false;
+    restClickDisabled = false;
+
 }
 
-playGame()
+function restGame(){
+    if (restClickDisabled) return;
+    clickDisabled = false;
+    console.log('rest game');
+    humanScore = 0;
+    computerScore = 0;
+    i = 0;
+    screen.innerHTML = ``;
+}
+
+
+const options = document.querySelectorAll('.option');
+const screen = document.querySelector('.screen');
+const restButton = document.querySelector('.rest-btn');
+restButton.addEventListener('click', restGame);
+
+options.forEach((option) => {
+    option.addEventListener('click', logScreenText);
+})
+console.log(screen)
+
+
+//playGame()
